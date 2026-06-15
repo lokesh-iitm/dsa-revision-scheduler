9873018f119d5facf9a135b72b73a4943596a2a4
Lokesh Prasad <lokeshprasad798@gmail.com>
2026-06-15 23:45:48 +0530

    Add problem form

--- app/static/script.js
+++ app/static/script.js
@@ -12,3 +12,40 @@ async function loadDashboard(){
 }
 
 loadDashboard();
+document
+.getElementById("problem-form")
+.addEventListener("submit", async function(e){
+
+    e.preventDefault();
+
+    const problem = {
+
+        title:
+            document.getElementById("title").value,
+
+        topic:
+            document.getElementById("topic").value,
+
+        difficulty:
+            document.getElementById("difficulty").value
+    };
+
+    const response = await fetch(
+        "/problem",
+        {
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify(problem)
+        }
+    );
+
+    const data = await response.json();
+
+    alert(data.message);
+
+    loadDashboard();
+});

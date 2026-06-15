797f08ab455a5759cbd22fb66561b67b9a7dba10
Lokesh Prasad <lokeshprasad798@gmail.com>
2026-06-15 23:05:20 +0530

    get revision ,put revision,get dashboard

--- app/routers/problems.py
+++ app/routers/problems.py
@@ -68,3 +68,70 @@ def get_problems():
     conn.close()
 
     return rows
+@router.get("/revisions/today")
+def revisions_today():
+
+    today = datetime.now().date().isoformat()
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    SELECT p.title,
+           p.topic,
+           p.difficulty,
+           r.revision_date
+    FROM revisions r
+    JOIN problems p
+    ON p.id = r.problem_id
+    WHERE r.revision_date <= ?
+    AND r.completed = 0
+    """, (today,))
+
+    rows = cursor.fetchall()
+
+    conn.close()
+
+    return rows
+@router.put("/revision/{revision_id}")
+def complete_revision(revision_id: int):
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    UPDATE revisions
+    SET completed = 1
+    WHERE id = ?
+    """, (revision_id,))
+
+    conn.commit()
+    conn.close()
+
+    return {
+        "message": "Revision completed"
+    }
+@router.get("/dashboard")
+def dashboard():
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT COUNT(*) FROM problems"
+    )
+    total_problems = cursor.fetchone()[0]
+
+    cursor.execute("""
+    SELECT COUNT(*)
+    FROM revisions
+    WHERE completed = 0
+    """)
+    pending = cursor.fetchone()[0]
+
+    conn.close()
+
+    return {
+        "total_solved_problems": total_problems,
+        "pending_revisions": pending
+    }
